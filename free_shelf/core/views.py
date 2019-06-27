c7d45602dca751e92c622eb90d206a9ed385fcc1
Meagan-M <meaganmakinson80@gmail.com>
2019-06-27 16:57:39 -0400

    add heroku to deploy website

--- free_shelf/core/views.py
+++ free_shelf/core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .models import Book, Category
+from core.models import Book, Category
 from django.views import generic
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    '''View function for home page of site'''
+
+    books = Book.objects.all()
+
+    context = {
+        'books' : books,
+    }
+    return render(request, 'index.html', context=context)
