93e54451f8f96d8a587bc900203313c70ae1894c
Meagan-M <meaganmakinson80@gmail.com>
2019-06-27 17:39:28 -0400

    maybe this will work

---
+++ core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200, help_text="Enter the Book Title")
+    author = models.CharField(max_length=200, help_text="Enter the Author Name")
+    description = models.TextField(max_length=3000, help_text='Type Book Description Here')
+    url = models.URLField(unique = True)
+    date_added = models.DateField(auto_now_add= True)
+
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    class Meta:
+        ordering = ['-date_added']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the Book Category")
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+            
