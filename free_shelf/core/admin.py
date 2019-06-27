0fa1b06bd5ef4f7b9b1e51bf80fdf6663cb5ffde
Meagan-M <meaganmakinson80@gmail.com>
2019-06-27 17:33:55 -0400

    removed extra layer

--- free_shelf/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from core.models import Book, Category
-admin.site.register(Book)
-admin.site.register(Category)
-# admin.site.register()
